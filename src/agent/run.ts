9a968eef7c521a4d34bcef49480ae28919dc752f
daizy <dziyu47@gmail.com>
2026-06-07 09:49:22 +0800

    feat:ai SDK adapt deepseek provider

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -1,28 +1,25 @@
 import {generateText, type ModelMessage} from 'ai';
-import { openai } from '@ai-sdk/openai';
+import { deepseek , createDeepSeek} from '@ai-sdk/deepseek';
+import { createOpenAI , openai} from '@ai-sdk/openai'; // 这是一个适配层，可以切换不同的模型
+import { OpenAI } from 'openai';  
 import { SYSTEM_PROMPT } from './system/prompt';    
 import type { AgentCallbacks } from '../types';
 import { tools, fileTools } from './tools';
 import { executeTool } from './executeTool';
-const MODEL_NAME = 'gpt-5-mini';
-
+import dotenv from 'dotenv'; //把项目根目录里的 .env 文件加载进 process.env 里
+dotenv.config();
+const MODEL_NAME = 'deepseek-chat';
 export async function runAgent(
-    userNessage: string,
+    userMessage: string,
     conversationHistory?: ModelMessage[],
     callbacks?: AgentCallbacks
 ): Promise<any> {
-    const { text, toolCalls} = await generateText({
-        model: openai(MODEL_NAME),
-        system: SYSTEM_PROMPT,
-        prompt: userNessage,
-        tools,
+    const{ text ,toolCalls } = await generateText({
+    model: deepseek('deepseek-reasoner'),
+    system: "You are a helpful assistant.",
+    prompt: userMessage,
 
     })
-    toolCalls.forEach(async (toolCall) => {
-        const { name, args } = toolCall;
-        const result = await executeTool(name, args);
-        console.log(result);
-    });
     console.log('Generated text:', text);
 }
-runAgent('')
+runAgent('hello,can you hear me?') 
